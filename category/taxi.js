6d94d78a82f60c543a71c0cfac179738236e3fe8
Peerapat Junkhaow <peerapat40595@gmail.com>
2018-06-01 17:44:58 +0700

    Add Taxi expense template

---
+++ category/taxi.js
@@ -0,0 +1,7 @@
+// Taxi
+var merchant = "Taxi"
+var total = 200
+var category = "Local Transportation"
+var project = "AXA-THAILAND - Advisor Zone Phase 3"
+var milestone = "Delivery Assurance"
+var comment = "Working late Taxi (31/05/2018)"
